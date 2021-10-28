31a75b7369252f5e6663f6a5bd5194fd4d4dbbdb
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-10-29 00:54:26 +0200

    comitted

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-
-const booksContainer = document.querySelector('.books-container')
-const cartForShopping = {
-    quantity: 0,
-    items: []
+const booksContainer = document.querySelector('.booksContainer')
+let ShoppingCart = document.querySelector(".bag-total")
+let shoppingCartList = [];
+let books = [];
+window.onload = ()=> {
+loadBooks()
 }
-
-const AddToCardButtton = () => {
-    const AddToCardButtton = document.querySelectorAll('.addCart')
-    AddToCardButtton.forEach(btn => btn.addEventListener('click', element => {
-        AddToCardButtton(element)
-    })
-    )
-}
-
+function loadBooks (){
 fetch('https://striveschool-api.herokuapp.com/books')
 .then(response => response.json())
-.then(data => booksContainer.innerHTML = data.map(book => `
+.then((data) => {
+  books = data 
+  displayBooks()
+})
+.catch(error => console.log(error))
+};
+
+
+function displayBooks(){
+  books.forEach((book) => {
+  booksContainer.innerHTML +=` 
 <div class= "col-12 col-sm-6 col-md-4 col-lg-3 py-3"
-<div class="card h-100">
-  <img src="${book.img}" class="card-img-top" alt="...">
-  <div class="card-body">
+<div class="card">
+<img src="${book.img}" class="card-img" alt="...">
+<div class="card-body">
     <h5 class="card-title">${book.title}</h5>
-    <p class="card-text">${book.price}</p>
-    <a href="#" class="btn btn-primary addCart">Add to cart</a>
-  </div>
-  </div>
-</div>`).join(''))
-.then(AddToCardButtton())
- .catch(error => error)
- const add = element =>{
-     
- }
+    <button onclick = "addToCart(${book.asin})"id ="add-cart" class="d-inline p-2 bg-success text-white">${book.price}</button>
+    <button class="d-inline p-2 bg-dark text-white">omit</button>
+    </div>
+    </div>
+    </div>`
+})
+};
+function addToCart(asin){
+  
+const book = books.find((book) =>book.asin ==asin)
+shoppingCartList.push(book)
+console.log(shoppingCartList)
+refreshShoppingCart()
+element.closest(".card").classList.add("selected");
+};
+
+function refreshShoppingCart(){
+  ShoppingCart.innerHTML =""
+
+  shoppingCartList.forEach((book) =>{
+    ShoppingCart.innerHTML +=`
+    <div class ="shopping-item">
+    
+    </div>
+    ${book.title}
+    </div>
+    </div>
+    ${book.price}
+    </div>
+    </div>
+    <button class="btn btn-danger" onclick="deleteItem('${String(
+      book.asin
+    )}')">Delete </button>
+    `
+  })
+};
