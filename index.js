9be0c2f3a688e6877afef13d796e97e4394f0ad6
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-10-28 16:55:38 +0200

    added some features

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 const booksContainer = document.querySelector('.books-container')
-
-const shoppingCart = {
+const cartForShopping = {
     quantity: 0,
     items: []
 }
 
 const AddToCardButtton = () => {
     const AddToCardButtton = document.querySelectorAll('.addCart')
-    AddToCardButtton.forEach(btn => btn.addEventListener('click', e => {
-        AddToCardButtton(e)
+    AddToCardButtton.forEach(btn => btn.addEventListener('click', element => {
+        AddToCardButtton(element)
     })
     )
 }
@@ -28,53 +27,7 @@ fetch('https://striveschool-api.herokuapp.com/books')
   </div>
 </div>`).join(''))
 .then(AddToCardButtton())
-// .then(data => booksContainer.innerHTML = data.map(book => `
-// <div class="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-//     <div class="card">
-//         <img src=${book.img} class="card book-image " alt="...">
-//         <div class="card-body">
-//             <p class="card-text font-weight-bold">${book.title}</p>
-//             <p class="card-text">${book.price}</p>
-//             <button class="btn btn-success addToCard">Add To cart</button>
-//         </div>
-//     </div>
-// </div>
-// `).join(''))
-// .then(AddToCardButtton)
-
-
-
-// const addToCart = e => {
-//     const btn = e.target
-//     btn.className = 'btn btn-outline-success'
-//     btn.innerText = 'Added'
-//     const price = btn.previousElementSibling.innerText
-//     const title = btn.previousElementSibling.previousElementSibling.innerText
-//     const image = btn.parentElement.previousElementSibling.src
-//     shoppingCart.quantity++
-//     shoppingCart.items.push({title, price, image})
-//     console.log(shoppingCart)
-//     updateCartNavbarDisplay()
-//     updateCartSection()
-// }
-
-// const updateCartNavbarDisplay = () => {
-//     const cartTotal = document.querySelector('.bag-total')
-//     cartTotal.innerText = shoppingCart.quantity
-// }
-    
-// const updateCartSection = () => {
-//     if (shoppingCart.quantity === 0) {
-//         cartContainer.innerHTML = `
-//         <div class="col-12 text-center">
-//             <h3>Your Cart Is Empty</h3>
-//         </div>`
-//     } else {
-//         cartContainer.innerHTML = shoppingCart.items.map(item => `
-//         <div class="col-12 d-flex justify-content-between">
-//             <p>${item.title}</p>
-//             <p>${item.price}</p>
-//         </div>
-//         `)
-//     }
-// }
+ .catch(error => error)
+ const add = element =>{
+     
+ }
